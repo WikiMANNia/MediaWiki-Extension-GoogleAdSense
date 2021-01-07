e03e32bc5512fc8e24b7b464a619b1b51e48f4af
WikiMANNia <55834825+WikiMANNia@users.noreply.github.com>
2021-01-07 08:41:49 +0100

    google_ad_width and google_ad_height can also be "auto"

--- GoogleAdSense.class.php
+++ GoogleAdSense.class.php
@@ -29,9 +29,13 @@ class GoogleAdSense {
 			return $bar;
 		}
 
-		if ( !$wgGoogleAdSenseSrc ) {
+		$width  = self::getAndCheckValue( $wgGoogleAdSenseWidth );
+		$height = self::getAndCheckValue( $wgGoogleAdSenseHeight );
+
+		if ( ( $width === false ) || ( $height === false ) || empty( $wgGoogleAdSenseSrc ) ) {
 			return $bar;
 		}
+
 
 		// Add CSS
 		$skin->getOutput()->addModules( 'ext.googleadsense' );
@@ -40,8 +44,8 @@ class GoogleAdSense {
 google_ad_client = \"$wgGoogleAdSenseClient\";
 /* $wgGoogleAdSenseID */
 google_ad_slot = \"$wgGoogleAdSenseSlot\";
-google_ad_width = " . intval( $wgGoogleAdSenseWidth ) . ";
-google_ad_height = " . intval( $wgGoogleAdSenseHeight ) . ";
+google_ad_width = " . $width . ";
+google_ad_height = " . $height . ";
 google_language = \"$wgGoogleAdSenseLang\";
 google_encoding = \"$wgGoogleAdSenseEncoding\";
 // -->
@@ -51,5 +55,22 @@ src=\"$wgGoogleAdSenseSrc\">
 </script>";
 
 		return true;
+	}
+
+	private static function getAndCheckValue( $value ) {
+
+		if ( empty( $value ) ) {
+			return false;
+		}
+
+		if ( $value === 'auto' ) {
+			return 'auto';
+		}
+
+		if ( is_int( $value ) ) {
+			return intval( $value );
+		}
+
+		return false;
 	}
 }
